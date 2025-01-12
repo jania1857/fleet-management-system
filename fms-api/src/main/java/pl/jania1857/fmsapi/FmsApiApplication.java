598c273a63fb9d33d4e1dafa65636473f992efff
Hubert Janiuk <hubert.janiuk@dajar.pl>
2025-01-12 20:37:26 +0100

    Initial commit

---
+++ fms-api/src/main/java/pl/jania1857/fmsapi/FmsApiApplication.java
@@ -0,0 +1,13 @@
+package pl.jania1857.fmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmsApiApplication.class, args);
+    }
+
+}
---
+++ fms-api/src/test/java/pl/jania1857/fmsapi/FmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jania1857.fmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
