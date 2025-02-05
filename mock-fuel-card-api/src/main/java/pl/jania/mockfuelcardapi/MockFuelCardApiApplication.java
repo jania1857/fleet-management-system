cf2e7ed2286ba2ca5b1a31a1ec8fc1dcb14b8ebe
hubert.janiuk <kombajndziki@gmai.om>
2025-02-05 09:08:33 +0100

    Fuel card mock api initialization

---
+++ mock-fuel-card-api/src/main/java/pl/jania/mockfuelcardapi/MockFuelCardApiApplication.java
@@ -0,0 +1,13 @@
+package pl.jania.mockfuelcardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockFuelCardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockFuelCardApiApplication.class, args);
+	}
+
+}
---
+++ mock-fuel-card-api/src/test/java/pl/jania/mockfuelcardapi/MockFuelCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jania.mockfuelcardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockFuelCardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
