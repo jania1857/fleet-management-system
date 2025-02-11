59889ec46e38e3f7a63b8774b25da4df36ec3413
hubert.janiuk <kombajndziki@gmai.om>
2025-02-11 13:31:36 +0100

    Insurance and inspections scheduled checking

--- fms-api/src/main/java/pl/jania1857/fmsapi/scheduler/Scheduler.java
+++ fms-api/src/main/java/pl/jania1857/fmsapi/scheduler/Scheduler.java
@@ -4,15 +4,23 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import pl.jania1857.fmsapi.dto.ChangeStatusRequest;
 import pl.jania1857.fmsapi.dto.shell.Transaction;
+import pl.jania1857.fmsapi.model.Inspection;
+import pl.jania1857.fmsapi.model.Insurance;
+import pl.jania1857.fmsapi.model.StatusChange;
 import pl.jania1857.fmsapi.model.Vehicle;
 import pl.jania1857.fmsapi.repository.VehicleRepository;
 import pl.jania1857.fmsapi.service.VehicleService;
 import pl.jania1857.fmsapi.utils.Fuel;
+import pl.jania1857.fmsapi.utils.Status;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class Scheduler {
@@ -23,6 +31,12 @@ public class Scheduler {
 
 
     private LocalDateTime lastFetchShell;
+
+    private final boolean shellFetchActive = false;
+    private final int shellFetchIntervalMs = 60 * 60 * 1000;
+
+    private final boolean vehiclesStatusCheckActive = true;
+    private final int vehiclesStatusCheckInterval = 24 * 60 * 60 * 1000;
 
     public Scheduler(
             VehicleService vehicleService,
@@ -36,8 +50,12 @@ public class Scheduler {
 
 
     //1-hour interval
-    @Scheduled(fixedRate = 3600000)
+    @Scheduled(fixedRate = shellFetchIntervalMs)
     public void fetchRefuelingsShell() {
+        if (!shellFetchActive) {
+            return;
+        }
+
         LocalDateTime lastFetch = LocalDateTime.now().minusHours(2);
         LocalDateTime now = LocalDateTime.now();
         if (lastFetchShell != null) {
@@ -88,4 +106,74 @@ public class Scheduler {
         lastFetchShell = now;
     }
 
+    @Scheduled(fixedRate = vehiclesStatusCheckInterval)
+    public void checkVehiclesStatus() {
+        if (!vehiclesStatusCheckActive) {
+            return;
+        }
+        LocalDateTime now = LocalDateTime.now();
+        vehicleRepository.findAll().forEach(vehicle -> {
+            boolean insuranceGood = checkInsurances(vehicle);
+            boolean inspectionGood = checkInspections(vehicle);
+
+            StatusChange latestStatusChange = vehicle.getStatusChanges().stream()
+                    .max(Comparator.comparing(StatusChange::getTimestamp)).orElse(null);
+
+            Status status;
+            if (latestStatusChange == null) {
+                status = Status.REQUIRES_ATTENTION;
+            } else {
+                status = latestStatusChange.getNewStatus();
+            }
+
+            if (insuranceGood && inspectionGood && (status != Status.READY && status != Status.ASSIGNED)) {
+                vehicleService.changeStatusForVehicle(vehicle.getId(), new ChangeStatusRequest(Status.READY));
+            }
+        });
+    }
+
+    private boolean checkInsurances(Vehicle vehicle) {
+        LocalDateTime now = LocalDateTime.now();
+        List<Insurance> insurances = vehicle.getInsurances();
+        if (insurances == null || insurances.isEmpty()) {
+            vehicleService.changeStatusForVehicle(vehicle.getId(), new ChangeStatusRequest(Status.BAD));
+            return false;
+        }
+        Insurance latestInsurance = insurances.stream()
+                .max(Comparator.comparing(Insurance::getEndDate)).orElse(null);
+
+        LocalDateTime latestInsuranceEndDate = latestInsurance.getEndDate();
+        if (latestInsuranceEndDate.isBefore(now)) {
+            vehicleService.changeStatusForVehicle(vehicle.getId(),
+                    new ChangeStatusRequest(Status.BAD));
+            return false;
+        } else if (latestInsuranceEndDate.isBefore(now.plusDays(14))) {
+            vehicleService.changeStatusForVehicle(vehicle.getId(),
+                    new ChangeStatusRequest(Status.REQUIRES_ATTENTION));
+            return false;
+        }
+        return true;
+    }
+    private boolean checkInspections(Vehicle vehicle) {
+        LocalDateTime now = LocalDateTime.now();
+        List<Inspection> inspections = vehicle.getInspections();
+        if (inspections == null || inspections.isEmpty()) {
+            vehicleService.changeStatusForVehicle(vehicle.getId(), new ChangeStatusRequest(Status.BAD));
+            return false;
+        }
+        Inspection latestInspection = inspections.stream()
+                .max(Comparator.comparing(Inspection::getInspectionDate)).orElse(null);
+
+        LocalDateTime latestInspectionDeadline = latestInspection.getNextInspectionDate();
+        if (latestInspectionDeadline.isBefore(now) || !latestInspection.getPassed()) {
+            vehicleService.changeStatusForVehicle(vehicle.getId(), new ChangeStatusRequest(Status.BAD));
+            return false;
+        }
+
+        if (latestInspectionDeadline.isBefore(now.plusDays(14))) {
+            vehicleService.changeStatusForVehicle(vehicle.getId(), new ChangeStatusRequest(Status.REQUIRES_ATTENTION));
+            return false;
+        }
+        return true;
+    }
 }
